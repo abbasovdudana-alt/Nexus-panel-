fab246a887d6702014111ac78814ce4d29712f90
abbasovdudana-alt <abbasovdudana@gmail.com>
2026-08-03 19:39:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
 
-import asyncio
 from datetime import datetime, timedelta
 import telebot
 from pyrogram import Client
